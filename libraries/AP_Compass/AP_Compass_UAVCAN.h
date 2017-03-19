e22f4a5173d3251ace7588114cd27faecbadf4ea
Eugene Shamaev <Eugene.Shamaev@gmail.com>
2017-03-19 12:54:41 +0200

    AP_Compass: support for UAVCAN connected magnetometers

---
+++ libraries/AP_Compass/AP_Compass_UAVCAN.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "AP_Compass.h"
+#include "AP_Compass_Backend.h"
+
+#include <AP_UAVCAN/AP_UAVCAN.h>
+
+class AP_Compass_UAVCAN : public AP_Compass_Backend
+{
+public:
+    void        read(void) override;
+
+    AP_Compass_UAVCAN(Compass &compass);
+    ~AP_Compass_UAVCAN() override;
+    // detect the sensor
+    static AP_Compass_Backend *detect(Compass &compass);
+
+    void handle_mag_msg(Vector3f &mag);
+
+private:
+    uint8_t  _instance;
+    int      _mag_fd;
+    Vector3f _sum;
+    uint32_t _count;
+    uint64_t _last_timestamp;
+};
